3394dc1fe61fa5aa85ffbf8a606642eea4d9c5cd
Pooja9783 <poojasankhala97@gmail.com>
2024-12-15 14:35:09 +0530

    created build

---
+++ src/components/Pagination/index.js
@@ -0,0 +1,12 @@
+import './styles.css'
+import React from 'react'
+
+export default function Pagination() {
+  return (
+    <div className='pagination-container'>
+        <div className='pagination-content'>
+        <p>Pagination</p>
+        </div> 
+    </div>
+  )
+}
