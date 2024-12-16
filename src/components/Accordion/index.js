f773b3096e730c65e56f80174d5635e24801a204
Pooja9783 <poojasankhala97@gmail.com>
2024-12-16 23:15:48 +0530

    implemneted basic accordion

--- src/components/Accordion/index.js
+++ src/components/Accordion/index.js
@@ -1,12 +1,31 @@
-import './styles.css'
-import React from 'react'
+import "./styles.css";
+import React, { useState } from "react";
 
 export default function Accordion() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleAccordionClick = () => {
+    setIsOpen(true);
+  };
+
+  const handleAccordion= () => {
+    setIsOpen(false);
+  };
+
   return (
-    <div className='accordion-container'>
-         <div className='accordion-content'>
-        <p>accordion</p>
-        </div> 
+    <div className="accordion-container">
+      <div className="accordion-content">
+        <div>Accordion</div>
+        <div className="accordion">
+          <p>One</p>
+          {isOpen ? (
+            <button onClick={handleAccordion}>-</button>
+          ) : (
+            <button onClick={handleAccordionClick}>+</button>
+          )}
+        </div>
+        <div>{isOpen && <p>dsfghhhhhhhhhhhhhhhhhhhhhh</p>}</div>
+      </div>
     </div>
-  ) 
+  );
 }
